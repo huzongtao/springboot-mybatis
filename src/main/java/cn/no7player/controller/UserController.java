cfb46ae0a242cac6b5e82fc0cea094fa9542cd23
huzongtao <zongtao_hu@163.com>
2017-06-06 14:52:54 +0800

    springboot实例

---
+++ src/main/java/cn/no7player/controller/UserController.java
@@ -0,0 +1,31 @@
+package cn.no7player.controller;
+
+import cn.no7player.model.User;
+import cn.no7player.service.UserService;
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by huzongtao on 2017/04/27.
+ */
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    private Logger logger = Logger.getLogger(UserController.class);
+
+    @Autowired
+    private UserService userService;
+
+    @RequestMapping("/getUserInfo")
+    public User getUserInfo() {
+        User user = userService.getUserInfo();
+        if(user!=null){
+            System.out.println("user.getName():"+user.getName());
+            logger.info("user.getAge():"+user.getAge());
+        }
+        return user;
+    }
+}
