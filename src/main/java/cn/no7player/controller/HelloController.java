ba4ff8bc2c8c48ef9bd3c8bc0506aa1788b22d42
huzongtao <zongtao_hu@163.com>
2017-06-29 14:18:53 +0800

    fix bug

--- src/main/java/cn/no7player/controller/HelloController.java
+++ src/main/java/cn/no7player/controller/HelloController.java
@@ -1,17 +1,19 @@
 package cn.no7player.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
-@Controller
+@RestController
+@RequestMapping("/index")
 public class HelloController {
 
     @RequestMapping("/hello")
-    public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
+    public ModelAndView greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
         model.addAttribute("name", name);
-        return "hello";
+        return new ModelAndView("error");
     }
-    
+
 }
